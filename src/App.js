ed82540d5a199720461b914ad0067f8bc49a97ed
Brennan <brennanduffey@gmail.com>
2019-05-16 09:30:46 -0600

    Add CardContainer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardContainer from './CardContainer';
 
 class App extends Component {
   constructor() {
@@ -22,7 +23,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+          <CardContainer reservations={this.state.reservations}/>
         </div>
       </div>
     )
---
+++ src/CardContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardContainer = ({reservations}) => {
+  const reservationCards = 9
+  return (
+    <section>
+    </section>
+  )
+}
+
+export default CardContainer
