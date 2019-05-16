0192c634ddb6da4504fbc710981be489cd661f0a
Brennan <brennanduffey@gmail.com>
2019-05-16 09:26:17 -0600

    Add fetch on component did mount

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+    .then(response => response.json())
+    .then(result => this.setState({ reservations: result }))
+  }
   render() {
     return (
       <div className="App">
